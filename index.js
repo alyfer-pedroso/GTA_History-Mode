9ff165a6b3c14560a8bc130efe048593ed716cac
alyfer-pedroso <alyferleandroaraujopedroso@gmail.com>
2023-08-22 19:51:43 -0300

    edit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// inGAME PROPERTIES
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
 const spriteRunRight = "./img/runRight (1).png";
@@ -9,361 +10,322 @@ const spriteJumpLeft = "./img/jumpLeft (1).png";
 const spritePlatformRoad = "./img/platformRoadB.png";
 const spriteBackground = "./img/background.png";
 const spriteBuildings = "./img/hills.png";
+let isGame = false;
 
 canvas.width = 1024;
 canvas.height = 574;
 
-class Player {
-    constructor() {
-        this.position = {
-            x: 100,
-            y: 0,
-        };
-        this.velocity = {
-            x: 0,
-            y: 0,
-        };
-        this.width = 118;
-        this.height = 120;
-        this.frames = 0;
-        this.sprites = {
-            idle: {
-                right: createSprite(spriteIdleRight),
-                left: createSprite(spriteIdleLeft),
-            },
-            run: {
-                right: createSprite(spriteRunRight),
-                left: createSprite(spriteRunLeft),
-            },
-            jump: {
-                right: createSprite(spriteJumpRight),
-                left: createSprite(spriteJumpLeft),
-            },
-        };
-        this.currentSprite = this.sprites.idle.right;
-        this.canJump = true;
-    }
+// inMENU PROPERTIES
+const $menu = document.querySelector(".menu");
+const $menu_btn = document.querySelector(".menu-btn");
 
-    draw() {
-        c.drawImage(this.currentSprite, 236 * this.frames, 0, 236, 240, this.position.x, this.position.y, this.width, this.height);
-    }
+$menu_btn.addEventListener("click", () => {
+    callGame();
+});
 
-    animate() {
-        this.frames++;
-        if (this.frames > 7) {
+function callGame() {
+    isGame = true;
+    $menu_btn.removeEventListener;
+    $menu.style.display = "none";
+
+    class Player {
+        constructor() {
+            this.position = {
+                x: 100,
+                y: 0,
+            };
+            this.velocity = {
+                x: 0,
+                y: 0,
+            };
+            this.width = 118;
+            this.height = 120;
             this.frames = 0;
+            this.sprites = {
+                idle: {
+                    right: createSprite(spriteIdleRight),
+                    left: createSprite(spriteIdleLeft),
+                },
+                run: {
+                    right: createSprite(spriteRunRight),
+                    left: createSprite(spriteRunLeft),
+                },
+                jump: {
+                    right: createSprite(spriteJumpRight),
+                    left: createSprite(spriteJumpLeft),
+                },
+            };
+            this.currentSprite = this.sprites.idle.right;
+            this.canJump = true;
         }
 
-        if (!this.canJump) {
+        draw() {
+            c.drawImage(this.currentSprite, 236 * this.frames, 0, 236, 240, this.position.x, this.position.y, this.width, this.height);
+        }
+
+        animate() {
             this.frames++;
-            if (this.frames > 4) {
+            if (this.frames > 7) {
                 this.frames = 0;
             }
-        }
-    }
-}
 
-class Platform {
-    constructor(x, y, image) {
-        this.position = {
-            x,
-            y,
-        };
-        this.image = image;
-        this.width = image.width;
-        this.height = image.height;
-    }
-
-    draw() {
-        c.drawImage(this.image, this.position.x, this.position.y);
-    }
-}
-
-class GenericObject {
-    constructor(x, y, image) {
-        this.position = {
-            x,
-            y,
-        };
-        this.image = image;
-        this.width = image.width;
-        this.height = image.height;
-    }
-
-    draw() {
-        c.drawImage(this.image, this.position.x, this.position.y);
-    }
-}
-
-// -----------------------------------------------------------------------------------------------
-
-const keys = {
-    right: { pressed: false },
-    left: { pressed: false },
-};
-let scrollOffset = 0;
-
-gravity = () => {
-    const gravity = 1.5;
-    player.position.x += player.velocity.x;
-    player.position.y += player.velocity.y;
-
-    if (player.position.y + player.height + player.velocity.y <= canvas.height) {
-        player.velocity.y += gravity;
-    } else {
-        // player.canJump = true;
-        if (player.currentSprite == player.sprites.jump.right) {
-            player.currentSprite = player.sprites.idle.right;
-        } else if (player.currentSprite == player.sprites.jump.left) {
-            player.currentSprite = player.sprites.idle.left;
-        }
-    }
-};
-
-playerMoviment = () => {
-    if (keys.right.pressed && player.position.x < 400) {
-        player.velocity.x = 5;
-        if (!player.canJump) {
-            player.currentSprite = player.sprites.jump.right;
-        } else {
-            player.currentSprite = player.sprites.run.right;
-        }
-    } else if ((keys.left.pressed && player.position.x > 100) || keys.left.pressed && scrollOffset === 0 && player.position.x > 0) {
-        player.velocity.x = -5;
-        if (!player.canJump) {
-            player.currentSprite = player.sprites.jump.left;
-        } else {
-            player.currentSprite = player.sprites.run.left;
-        }
-    } else {
-        player.velocity.x = 0;
-
-        if (keys.right.pressed) {
-            if (!player.canJump) {
-                player.currentSprite = player.sprites.jump.right;
-            } else {
-                player.currentSprite = player.sprites.run.right;
+            if (!this.canJump) {
+                this.frames++;
+                if (this.frames > 4) {
+                    this.frames = 0;
+                }
             }
-            platforms.forEach((platform) => {
-                platform.position.x -= 5;
-            });
-            genericObjects.forEach((genericObject) => {
-                genericObject.position.x -= 3;
-            });
-            scrollOffset += 5
-        } else if (keys.left.pressed && scrollOffset > 0) {
-            if (!player.canJump) {
-                player.currentSprite = player.sprites.jump.left;
-            } else {
-                player.currentSprite = player.sprites.run.left;
-            }
-            platforms.forEach((platform) => {
-                platform.position.x += 5;
-            });
-            genericObjects.forEach((genericObject) => {
-                genericObject.position.x += 3;
-            });
-            scrollOffset -= 5
         }
     }
-};
 
-playerJump = () => {
-    if (player.canJump) {
-        player.velocity.y -= 25;
-        player.canJump = false;
+    class Platform {
+        constructor(x, y, image) {
+            this.position = {
+                x,
+                y,
+            };
+            this.image = image;
+            this.width = image.width;
+            this.height = image.height;
+        }
+
+        draw() {
+            c.drawImage(this.image, this.position.x, this.position.y);
+        }
     }
 
-    if (player.currentSprite == player.sprites.idle.right) {
-        player.currentSprite = player.sprites.jump.right;
-    } else if (player.currentSprite == player.sprites.idle.left) {
-        player.currentSprite = player.sprites.jump.left;
-    }
-};
+    class GenericObject {
+        constructor(x, y, image) {
+            this.position = {
+                x,
+                y,
+            };
+            this.image = image;
+            this.width = image.width;
+            this.height = image.height;
+        }
 
-playerCollision = () => {
-    //player with platform
-    platforms.forEach((platform) => {
-        platform.draw();
-        if (player.position.y + player.height <= platform.position.y + 35 && player.position.y + player.height + player.velocity.y >= platform.position.y + 35 && player.position.x + player.width >= platform.position.x && player.position.x <= platform.position.x + platform.width) {
-            player.velocity.y = 0;
-            player.canJump = true;
+        draw() {
+            c.drawImage(this.image, this.position.x, this.position.y);
+        }
+    }
+
+    // -----------------------------------------------------------------------------------------------
+
+    const keys = {
+        right: { pressed: false },
+        left: { pressed: false },
+    };
+    let scrollOffset = 0;
+
+    gravity = () => {
+        const gravity = 1.5;
+        player.position.x += player.velocity.x;
+        player.position.y += player.velocity.y;
+
+        if (player.position.y + player.height + player.velocity.y <= canvas.height) {
+            player.velocity.y += gravity;
+        } else {
+            // player.canJump = true;
             if (player.currentSprite == player.sprites.jump.right) {
                 player.currentSprite = player.sprites.idle.right;
             } else if (player.currentSprite == player.sprites.jump.left) {
                 player.currentSprite = player.sprites.idle.left;
             }
         }
-    });
+    };
 
-    //lose
-    if(player.position.y > canvas.height) {
-        start();
-    }
-};
+    playerMoviment = () => {
+        if (keys.right.pressed && player.position.x < 400) {
+            player.velocity.x = 5;
+            if (!player.canJump) {
+                player.currentSprite = player.sprites.jump.right;
+            } else {
+                player.currentSprite = player.sprites.run.right;
+            }
+        } else if ((keys.left.pressed && player.position.x > 100) || (keys.left.pressed && scrollOffset === 0 && player.position.x > 0)) {
+            player.velocity.x = -5;
+            if (!player.canJump) {
+                player.currentSprite = player.sprites.jump.left;
+            } else {
+                player.currentSprite = player.sprites.run.left;
+            }
+        } else {
+            player.velocity.x = 0;
 
-createSprite = (imgSrc) => {
-    image = new Image();
-    image.src = imgSrc;
-    return image;
-};
+            if (keys.right.pressed) {
+                if (!player.canJump) {
+                    player.currentSprite = player.sprites.jump.right;
+                } else {
+                    player.currentSprite = player.sprites.run.right;
+                }
+                platforms.forEach((platform) => {
+                    platform.position.x -= 5;
+                });
+                genericObjects.forEach((genericObject) => {
+                    genericObject.position.x -= 3;
+                });
+                scrollOffset += 5;
+            } else if (keys.left.pressed && scrollOffset > 0) {
+                if (!player.canJump) {
+                    player.currentSprite = player.sprites.jump.left;
+                } else {
+                    player.currentSprite = player.sprites.run.left;
+                }
+                platforms.forEach((platform) => {
+                    platform.position.x += 5;
+                });
+                genericObjects.forEach((genericObject) => {
+                    genericObject.position.x += 3;
+                });
+                scrollOffset -= 5;
+            }
+        }
+    };
 
-let playerFrames = setInterval(() => {
-    player.animate();
-}, 60);
+    playerJump = () => {
+        if (player.canJump) {
+            player.velocity.y -= 25;
+            player.canJump = false;
+        }
 
-platformRoad = createSprite(spritePlatformRoad);
+        if (player.currentSprite == player.sprites.idle.right) {
+            player.currentSprite = player.sprites.jump.right;
+        } else if (player.currentSprite == player.sprites.idle.left) {
+            player.currentSprite = player.sprites.jump.left;
+        }
+    };
 
-let player = new Player();
+    playerCollision = () => {
+        //player with platform
+        platforms.forEach((platform) => {
+            platform.draw();
+            if (player.position.y + player.height <= platform.position.y + 35 && player.position.y + player.height + player.velocity.y >= platform.position.y + 35 && player.position.x + player.width >= platform.position.x && player.position.x <= platform.position.x + platform.width) {
+                player.velocity.y = 0;
+                player.canJump = true;
+                if (player.currentSprite == player.sprites.jump.right) {
+                    player.currentSprite = player.sprites.idle.right;
+                } else if (player.currentSprite == player.sprites.jump.left) {
+                    player.currentSprite = player.sprites.idle.left;
+                }
+            }
+        });
 
-let platforms = [
-    new Platform(
-        -1, 
-        374, 
-        platformRoad
-        ), 
-    new Platform(
-        platformRoad.width - 2, 
-        374, 
-        platformRoad
-    ), 
-    new Platform(
-        platformRoad.width * 2 - 2, 
-        374, 
-        platformRoad
-    )
-];
+        //lose
+        if (player.position.y > canvas.height) {
+            start();
+        }
+    };
 
-let genericObjects = [
-    new GenericObject(
-        -1,
-        -200,
-        createSprite(spriteBackground)
-    ),
-    new GenericObject(
-        -1,
-        -210,
-        createSprite(spriteBuildings)
-    )
-]
+    createSprite = (imgSrc) => {
+        image = new Image();
+        image.src = imgSrc;
+        return image;
+    };
 
-// -----------------------------------------------------------------------------------------------
+    let playerFrames = setInterval(() => {
+        player.animate();
+    }, 60);
 
-function start() {
-    scrollOffset = 0;
     platformRoad = createSprite(spritePlatformRoad);
 
-    player = new Player();
-    
-    platforms = [
-        new Platform(
-            -1, 
-            374, 
-            platformRoad
-            ), 
-        new Platform(
-            platformRoad.width - 2, 
-            374, 
-            platformRoad
-        ), 
-        new Platform(
-            platformRoad.width * 2 - 2, 
-            374, 
-            platformRoad
-        )
-    ];
-    
-    genericObjects = [
-        new GenericObject(
-            -1,
-            -200,
-            createSprite(spriteBackground)
-        ),
-        new GenericObject(
-            -1,
-            -210,
-            createSprite(spriteBuildings)
-        )
-    ]
-}
+    let player = new Player();
 
-function update() {
-    requestAnimationFrame(update);
-    c.fillStyle = "white";
-    c.fillRect(0, 0, canvas.width, canvas.height);
-    gravity();
+    let platforms = [new Platform(-1, 374, platformRoad), new Platform(platformRoad.width - 2, 374, platformRoad), new Platform(platformRoad.width * 2 - 2, 374, platformRoad)];
 
-    genericObjects.forEach((genericObject)=> {
-        genericObject.draw()
-    })
-    platforms.forEach((platform) => {
-        platform.draw();
+    let genericObjects = [new GenericObject(-1, -200, createSprite(spriteBackground)), new GenericObject(-1, -210, createSprite(spriteBuildings))];
+
+    // -----------------------------------------------------------------------------------------------
+
+    function start() {
+        scrollOffset = 0;
+        platformRoad = createSprite(spritePlatformRoad);
+
+        player = new Player();
+
+        platforms = [new Platform(-1, 374, platformRoad), new Platform(platformRoad.width - 2, 374, platformRoad), new Platform(platformRoad.width * 2 - 2, 374, platformRoad)];
+
+        genericObjects = [new GenericObject(-1, -200, createSprite(spriteBackground)), new GenericObject(-1, -210, createSprite(spriteBuildings))];
+    }
+
+    function update() {
+        requestAnimationFrame(update);
+        c.fillStyle = "white";
+        c.fillRect(0, 0, canvas.width, canvas.height);
+        gravity();
+
+        genericObjects.forEach((genericObject) => {
+            genericObject.draw();
+        });
+        platforms.forEach((platform) => {
+            platform.draw();
+        });
+
+        playerCollision();
+        player.draw();
+        playerMoviment();
+    }
+
+    // -----------------------------------------------------------------------------------------------
+
+    addEventListener("keydown", ({ key }) => {
+        switch (key) {
+            case "w":
+                playerJump();
+                break;
+
+            case "ArrowUp":
+                playerJump();
+                break;
+
+            case " ":
+                playerJump();
+                break;
+
+            case "a":
+                keys.left.pressed = true;
+                player.currentSprite = player.sprites.run.left;
+                break;
+
+            case "d":
+                keys.right.pressed = true;
+                player.currentSprite = player.sprites.run.right;
+                break;
+
+            case "ArrowLeft":
+                keys.left.pressed = true;
+                player.currentSprite = player.sprites.run.left;
+                break;
+
+            case "ArrowRight":
+                keys.right.pressed = true;
+                player.currentSprite = player.sprites.run.right;
+                break;
+        }
     });
+    addEventListener("keyup", ({ key }) => {
+        switch (key) {
+            case "a":
+                keys.left.pressed = false;
+                player.currentSprite = player.sprites.idle.left;
+                break;
 
-    playerCollision();
-    player.draw();
-    playerMoviment();
+            case "d":
+                keys.right.pressed = false;
+                player.currentSprite = player.sprites.idle.right;
+                break;
+
+            case "ArrowLeft":
+                keys.left.pressed = false;
+                player.currentSprite = player.sprites.idle.left;
+                break;
+
+            case "ArrowRight":
+                keys.right.pressed = false;
+                player.currentSprite = player.sprites.idle.right;
+                break;
+        }
+    });
+    window.addEventListener("load", start());
+    window.setInterval(update(), 1000 / 60);
 }
-
-// -----------------------------------------------------------------------------------------------
-
-addEventListener("keydown", ({ key }) => {
-    switch (key) {
-        case "w":
-            playerJump();
-            break;
-
-        case "ArrowUp":
-            playerJump();
-            break;
-
-        case " ":
-            playerJump();
-            break;
-
-        case "a":
-            keys.left.pressed = true;
-            player.currentSprite = player.sprites.run.left;
-            break;
-
-        case "d":
-            keys.right.pressed = true;
-            player.currentSprite = player.sprites.run.right;
-            break;
-
-        case "ArrowLeft":
-            keys.left.pressed = true;
-            player.currentSprite = player.sprites.run.left;
-            break;
-
-        case "ArrowRight":
-            keys.right.pressed = true;
-            player.currentSprite = player.sprites.run.right;
-            break;
-    }
-});
-addEventListener("keyup", ({ key }) => {
-    switch (key) {
-        case "a":
-            keys.left.pressed = false;
-            player.currentSprite = player.sprites.idle.left;
-            break;
-
-        case "d":
-            keys.right.pressed = false;
-            player.currentSprite = player.sprites.idle.right;
-            break;
-
-        case "ArrowLeft":
-            keys.left.pressed = false;
-            player.currentSprite = player.sprites.idle.left;
-            break;
-
-        case "ArrowRight":
-            keys.right.pressed = false;
-            player.currentSprite = player.sprites.idle.right;
-            break;
-    }
-});
-window.addEventListener('load', start());
-window.setInterval(update(), 1000 / 60);
